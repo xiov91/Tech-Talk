404ef88da5eedd6fda3bfd72a5dac9ffaa8605ea
xiov91 <99517717+xiov91@users.noreply.github.com>
2022-09-07 13:58:46 -0400

    post routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,60 +1,72 @@
-const router = require("express").Router();
-const { Post, Comment, User } = require("../../models/");
-const withAuth = require("../../utils/auth");
+const router = require('express').Router();
+const sequelize = require('../config/config');
+const { Post } = require('../../models/');
+const withAuth = require('../../utils/auth');
 
 //create a new post
 // http://localhost:3000/api/post
-router.post("/", withAuth, (req, res) => {
-	const body = req.body;
-	// console.log(req.session.userId);
+router.post('/', withAuth, (req, res) => {
+
 	Post.create({
 		title: req.body.title,
-		body: req.body.body
+		post_text: req.body.post_text,
+		user_id: req.session.user_id
 	})
-		.then((data) => {
-			res.json(data);
-		})
-		.catch((err) => {
+		.then((postData) => res.json(postData))
+		.catch(err => {
+			console.log(err);
 			res.status(500).json(err);
 		});
 });
+
 // update a post
 // http://localhost:3000/api/post/:id
-router.put("/:id", withAuth, (req, res) => {
-	Post.update(req.body, {
-		where: {
-			id: req.params.id
+router.put('/:id', withAuth, (req, res) => {
+	Post.update(
+		{
+			title: req.body.title,
+			post_text: req.body.post_text
 		},
-	})
-		.then((data) => {
-			if (data > 0) {
-				res.status(200).end();
-			} else {
-				res.status(404).end();
+		{
+			where: {
+				id: req.params.id
 			}
+		}
+	)
+		.then((postData) => {
+			if (!postData) {
+				res.status(404).json({ message: `This isn't the post you're looking for (bad ID)` });
+				return;
+			}
+			res.json(postData);
 		})
-		.catch((err) => {
+		.catch(err => {
+			console.log(err);
 			res.status(500).json(err);
 		});
 });
+
 // delete a post
 // http://localhost:3000/api/post/:id
-router.delete("/:id", withAuth, (req, res) => {
+router.delete('/:id', withAuth, (req, res) => {
+	console.log('id', req.params.id);
 	Post.destroy({
 		where: {
 			id: req.params.id
-		},
+		}
 	})
-		.then((data) => {
-			if (data > 0) {
-				res.status(200).end();
-			} else {
-				res.status(404).end();
+		.then(postData => {
+			if (!postData) {
+				res.status(404).json({ message: `This isn't the post you're looking for (bad ID)` });
+				return;
 			}
+			res.json(postData);
 		})
-		.catch((err) => {
+		.catch(err => {
+			console.log(err);
 			res.status(500).json(err);
 		});
 });
+
 
 module.exports = router;
