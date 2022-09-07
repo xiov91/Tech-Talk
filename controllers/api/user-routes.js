0654c0553c88897c02293fa2c0e748cd7c2967c5
xiov91 <99517717+xiov91@users.noreply.github.com>
2022-09-07 14:07:18 -0400

    user routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,23 +1,23 @@
-const router = require("express").Router();
-const { User } = require("../../models");
+const router = require('express').Router();
+const { User, Post, Comment } = require('../../models');
 
 // post a new user
 // http://localhost:3000/api/users
-router.post("/", (req, res) => {
+router.post('/', (req, res) => {
 	User.create({
 		username: req.body.username,
 		password: req.body.password
 	})
-		.then((data) => {
+		.then((userData) => {
 			req.session.save(() => {
-				req.session.id = data.id;
-				req.session.username = data.username;
+				req.session.id = userData.id;
+				req.session.username = userData.username;
 				req.session.loggedIn = true;
 
-				res.json(data);
-			})
+				res.json(userData);
+			});
 		})
-		.catch((err) => {
+		.catch(err => {
 			console.log(err);
 			res.status(500).json(err);
 		});
@@ -25,62 +25,60 @@ router.post("/", (req, res) => {
 
 // login a user
 // http://localhost:3000/api/users/login
-router.post("/login", (req, res) => {
+router.post('/login', (req, res) => {
 	User.findOne({
 		where: {
 			username: req.body.username
 		},
-	}).then((data) => {
-		if (!data) {
-			res.status(400).json({ message: "No user account found!" });
+	}).then((userData) => {
+		if (!userData) {
+			res.status(400).json({ message: 'No user account found!' });
 			return;
 		}
-		// it is good during development to log a sucessful login
-		const validPassword = data.checkPassword(req.body.password);
-		// it is good during development to log a un sucessful login reason
+
+		const validPassword = userData.checkPassword(req.body.password);
+
 		if (!validPassword) {
-			res.status(400).json({ message: "Incorrect password!" });
+			res.status(400).json({ message: 'Incorrect password!' });
 			return;
 		}
 
 		req.session.save(() => {
-			req.session.id = data.id;
-				req.session.username = data.username;
+			req.session.id = userData.id;
+				req.session.username = userData.username;
 				req.session.loggedIn = true;
-			// great to have this message
-			res.json({ user: data, message: "You are now logged in!" });
+			
+			res.json({ user: userData, message: 'You are now logged in!' });
 		});
 	});
 });
 
 // logout user
 // http://localhost:3000/api/users/logout
-router.post("/logout", (req, res) => {
+router.post('/logout', (req, res) => {
 	if (req.session.loggedIn) {
     req.session.destroy(() => {
-      // it is important to end() the destruction of the session data here, otherwise you app could stall 
-			res.status(204).json({ message: "You are now logged out!" }).end();
+			res.status(204).json({ message: 'You are now logged out!' }).end();
 		});
   } else {
-    // you will commonly see the end() with 404s
 		res.status(400).end();
 	}
 });
 
 // delete a user
 // http://localhost:3000/api/users/user/:id
-router.delete("/user/:id", (req, res) => {
+router.delete('/user/:id', (req, res) => {
 	User.destroy({
 		where: {
 			id: req.params.id
 		},
 	})
-		.then((data) => {
-			if (!data) {
-				res.status(404).json({ message: "No user found with this id" });
+		.then((userData) => {
+			if (!userData) {
+				res.status(404).json({ message: 'No user found with this id' });
 				return;
 			}
-			res.json(data);
+			res.json(userData);
 		})
 		.catch((err) => {
 			console.log(err);
